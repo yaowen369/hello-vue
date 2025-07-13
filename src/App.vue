4dc11a3919e64d990e8e738808e4cafc77a76985
yaowen <yaowen@bwton.com>
2025-07-13 16:07:37 +0800

    最基础的知识

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      {{message}}
+    </div>
+
+    <div id="app-2">
+      <span v-bind:title="message2">
+        鼠标悬停几秒钟查看此处动态绑定的提示信息！
+      </span>
+    </div>
+
+    <div id="app-5">
+      <button v-on:click="reverseMessage">反转消息</button>
+    </div>
+
+    <div id="app-6">
+      <p>message</p>
+      <input v-model="message">
+    </div>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
@@ -9,6 +27,17 @@ import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!',
+      message2: '页面加载于 ' + new Date().toLocaleString()
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
   components: {
     HelloWorld
   }
