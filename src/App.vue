e9fd3275114d3f65e7797b58bda525aaf84ba346
yaowen <yaowen@bwton.com>
2025-07-14 09:16:40 +0800

    提交修改

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,23 @@
       <p>message</p>
       <input v-model="message">
     </div>
+
+    <div id="app-7">
+      <p>通过计算属性,来自动的反转message : {{reverseMessage}}</p>
+    </div>
+
+<!--    这个组件始终没有引用成功，还没找到原因 -->
+<!--    <ol>-->
+<!--      <todo-item></todo-item>-->
+<!--    </ol>-->
+
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
   name: 'App',
@@ -33,12 +44,34 @@ export default {
       message2: '页面加载于 ' + new Date().toLocaleString()
     }
   },
+
+  beforeCreate:function () {
+    console.log('生命周期函数, beforeCreate'+ "," + this);
+   },
+  mounted: function () {
+    console.log('生命周期函数, mounted'+ "," + this);
+  },
+  created: function () {
+    console.log('生命周期函数, created' + "," + this);
+  },
+
+  updated:function () {
+    console.log('生命周期函数, updated');
+   },
+  destroyed:function () {
+    console.log('生命周期函数, destroyed');
+   },
   methods: {
+  },
+
+  computed: {
     reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+      console.log('计算属性 computed, ');
+      return  this.message.split('').reverse().join('')
     }
   },
   components: {
+    TodoItem,
     HelloWorld
   }
 };
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,4 @@
+
+export default Vue.extend({
+  template: '<li>这是个待办项</li>'
+})
